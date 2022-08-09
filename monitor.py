82d4e803a38a955deca6cc473be5e59fe68e3ff8
CharlieAIO <charliebottomley15@gmail.com>
2022-08-09 18:52:08 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -97,8 +97,8 @@ def createSession():
         captcha={"provider": 'vanaheim'}
     )
     helheim.wokou(session)
-    # session.bifrost_clientHello = 'chrome'
-    # helheim.bifrost(session, './bifrost-0.0.7-linux.x86_64.so')
+    session.bifrost_clientHello = 'chrome'
+    helheim.bifrost(session, './bifrost-0.0.7-linux.x86_64.so')
     session.headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     session.headers['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
     return session
